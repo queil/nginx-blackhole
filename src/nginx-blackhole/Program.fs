9aa1c943682b5c8de422ecd7aeb2d85c14620aab
queil <queil@users.noreply.github.com>
2019-12-26 17:37:39 +0000

    Version 0

---
+++ src/nginx-blackhole/Program.fs
@@ -0,0 +1,46 @@
+﻿open System
+open System.Threading
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Headers
+
+[<EntryPoint>]
+let main argv = 
+  let cts = new CancellationTokenSource()
+  let conf = { defaultConfig with cancellationToken = cts.Token }
+   
+  let passThruHeaders : WebPart =
+    fun (ctx:HttpContext) -> 
+       async { 
+           let passThruHeader name = 
+            match ctx |> getFirstHeader name with
+             | Some value -> (name, value)::ctx.response.headers |> ignore
+             | None -> ()  
+
+           Headers.All |> Seq.iter passThruHeader
+           return Some ctx 
+        } 
+  let setStatusCodeFromXCodeHeader : WebPart =
+    fun (ctx:HttpContext) -> 
+        let newCode =  match ctx |> getFirstHeader Headers.CodeHeader with
+                        | Some value -> Int32.Parse(value)
+                        | None -> 404
+        { ctx with response = {ctx.response with status = {ctx.response.status with code = newCode }} } |> succeed   
+
+  let app =
+    choose [
+        GET >=> choose
+            [ path "/healthz" >=> OK ""
+              path "/" >=> passThruHeaders >=> setStatusCodeFromXCodeHeader ]
+        ]
+  
+  let listening, server = startWebServerAsync conf app
+    
+  Async.Start(server, cts.Token)
+  
+  Console.CancelKeyPress.Add(fun _ -> cts.Cancel())
+  Async.AwaitEvent Console.CancelKeyPress |> Async.RunSynchronously |> ignore 
+
+  0 // return an integer exit code
